c607e64bdc978a44094f75b524879e795b7194eb
Mark Alindogan <marleoali_777@yahoo.com>
2021-07-05 21:51:31 -0500

    base navbar

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,16 +2,17 @@ import { useEffect, useState } from 'react';
 import Header from './HeaderComponent';
 import Home from './HomeComponent';
 import Footer from './FooterComponent';
+import { render } from '@testing-library/react';
 
 function Main() {
 
-    return (
-        <div>
-            <Header />
-            <Home />
-            <Footer />
-        </div>
-    );
+        return (
+            <div>
+                <Header />
+                <Home />
+            </div>
+        );
+
 }
 
-export default (Main);
+export default Main;
